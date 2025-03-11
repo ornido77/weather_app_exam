084ed896e68a97b5a614c2741c8fb7e73eef7d99
Sonny Ornido <sonny.ornido@ximera.com.ph>
2025-03-11 09:43:30 +0800

    FIX: Redundant apiKey in WeatherRepository.

--- lib/core/repositories/weather_repository.dart
+++ lib/core/repositories/weather_repository.dart
@@ -5,11 +5,10 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app_exam/core/models/weather.dart';
 
 class WeatherRepository {
-  final String apiKey = dotenv.env['API_KEY'] ?? '';
   final String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
   Future<Weather> fetchWeather(String city, {bool isXml = false}) async {
-    final apiKey = dotenv.env['API_KEY'];
+    final String apiKey = dotenv.env['API_KEY'] ?? '';
     final uri = Uri.parse(
       "$baseUrl?q=$city&appid=$apiKey&units=metric${isXml ? '&mode=xml' : ''}",
     );
