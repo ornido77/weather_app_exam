80f6560b5d68f35a16bc68be640da9e5d16b8064
Sonny Ornido <sonny.ornido@ximera.com.ph>
2025-03-11 10:22:20 +0800

    FIX

--- lib/core/repositories/weather_repository.dart
+++ lib/core/repositories/weather_repository.dart
@@ -5,10 +5,11 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app_exam/core/models/weather.dart';
 
 class WeatherRepository {
-  final String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
-
   Future<Weather> fetchWeather(String city, {bool isXml = false}) async {
     final String apiKey = dotenv.env['API_KEY'] ?? '';
+
+    final String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
     final uri = Uri.parse(
       "$baseUrl?q=$city&appid=$apiKey&units=metric${isXml ? '&mode=xml' : ''}",
     );
